ce96472042cd4c7598f024b1d11c859efd908bc9
Hirvio <oscar.rehnstrom@hotmail.se>
2014-01-16 03:23:06 +0100

    Initial Commit

---
+++ Hammer Age of Watchelot/Hammer Age of Watchelot/Hammer Age of Watchelot/Program.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Hammer_Age_of_Watchelot
+{
+#if WINDOWS || XBOX
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            using (Main game = new Main())
+            {
+                game.Run();
+            }
+        }
+    }
+#endif
+}
+
